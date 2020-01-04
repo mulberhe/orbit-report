ca9bb7cabdd15a6d6c87b463a339a523ebc8e911
mulberhe <mulfish7@yahoo.com>
2020-01-03 22:33:10 -0600

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
+import { OrbitCountComponent } from './orbit-count/orbit-count.component';
+import { OrbitCountsComponent } from './orbit-counts/orbit-counts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrbitListComponent,
+    OrbitCountsComponent
   ],
   imports: [
     BrowserModule
